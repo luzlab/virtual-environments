c61d1d7e4b84a8997f90b1ab815be6415b2bcf2c
Carlo Quinonez <carlo.quinonez@thermofisher.com>
2021-02-19 17:40:59 -0800

    Construct icacls grant clause properly

--- images/win/scripts/Installers/Install-Meteor.ps1
+++ images/win/scripts/Installers/Install-Meteor.ps1
@@ -28,8 +28,8 @@ meteor update --release 1.8.0
 robocopy "C:\Users\builderadmin.testbuilder1\AppData\Local\.meteor" "C:\Users\All Users\AppData\Local\.meteor" /MIR /SEC /XJD /R:5 /W:5 /MT:32 /V /NP
 
 # Update the permissions on the copied files so all users can execute and modify them
-# Since PowerShell assigns meaning to the `(` and `)`, we have to wrap the call to icacls in `Invoke-Command`
-Invoke-Expression -Command ('icacls "C:\Users\All Users\AppData\Local\.meteor" /grant All:(OI)(CI)F /t /c')
+# Since PowerShell assigns meaning to the `(` and `)`, escape with ` (backtick)
+icacls "C:\Users\All Users\AppData\Local\.meteor" /grant ("All"+":(OI)(CI)F") /t /c
 
 # Add the new Meteor installation into the PATH for all users. 
-'reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d "%path%;C:\Users\All Users\AppData\Local\.meteor\"'
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d "%path%;C:\Users\All Users\AppData\Local\.meteor\"
