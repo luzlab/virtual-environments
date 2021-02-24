1dae0ad34436ca993d9168d3315e4d803b2a59ca
Carlo Quinonez <carlo.quinonez@thermofisher.com>
2021-02-23 23:01:31 -0800

    Remove prefetching of meteor devbundles.

--- images/win/scripts/Installers/Install-Meteor.ps1
+++ images/win/scripts/Installers/Install-Meteor.ps1
@@ -5,25 +5,6 @@
 
 Choco-Install -PackageName meteor
 
-# Since PATH has changed, we'll reload so the current shell can use it.
-RefreshEnv
-
-# Prefetch the recent Meteor releases
-meteor update --release 2.0
-meteor update --release 1.12
-meteor update --release 1.11
-meteor update --release 1.10.3
-meteor update --release 1.10.2
-meteor update --release 1.10.1
-meteor update --release 1.9.3
-meteor update --release 1.9.2
-meteor update --release 1.9.1
-meteor update --release 1.9
-meteor update --release 1.8.3
-meteor update --release 1.8.2
-meteor update --release 1.8.1
-meteor update --release 1.8.0
-
 # Copy the meteor data to `All Users\AppData`
 robocopy "$env:LOCALAPPDATA\.meteor" "C:\Users\All Users\AppData\Local\.meteor" /MIR /SEC /XJD /R:5 /W:5 /MT:32 /V /NP
 
@@ -36,4 +17,4 @@ $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentC
 $newpath = "$oldpath;C:\Users\All Users\AppData\Local\.meteor"
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
 
-# The path only takes effect after logging out and back in
+# The path only takes effect at LogIn
